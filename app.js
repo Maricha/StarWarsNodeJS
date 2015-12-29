3c943830b2db9ca914a642f63dfde0d4033e1c86
Maricha <maricha177@gazeta.pl>
2015-12-29 21:27:26 +0100

    No i koniec

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var routes = require('./routes');
+app.set('view engine', 'ejs');
+
+//Statyczne
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Routes
+//home
+app.get('/', routes.home);
+
+//pojedyncze
+
+app.get('/star_wars_episode/:episode_number?', routes.movie_single);
+
+//NotFound
+app.get('*', routes.notFound);
+
+
+app.listen(process.env.PORT || 3000);
